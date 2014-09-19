0decbdd1ca22736ee0a45294a83484f1de760dd9
Mandi Grant <mandi018@gmail.com>
2014-09-18 19:22:20 -0700

    Basic setup

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.initConfig({
+    clean: {
+      dev: {
+        src: 'build/'
+      }
+    },
+    copy: {
+      dev: {
+        expand: true,
+        cwd: 'app/',  //current working directory
+        src: ['*.html', 'css/*.css', 'views/**/*.html'],
+        dest: 'build/',
+        filter: 'isFile'
+      }
+    },
+    browserify: {
+      dev: {
+        options: {
+          transform: ['debowerify'],
+          debug: true
+        },
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js'
+      },
+      angulartest: {
+        options: {
+          transform: ['debowerify'],
+          debug: true
+        },
+        src: ['test/angular/**/*test.js'],
+        dest: 'test/angular-testbundle.js'
+      }
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
+
+    express: {
+      options: {
+        port: 3000
+      },
+      dev: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
+
+    watch: {
+      angulartest: {
+        files: ['app/js/**/*.js', 'app/index.html', 'app/views/**/*.html'],
+        tasks: ['browserify:angulartest'],
+        options: {
+          spawn:false
+        }
+      },
+      express: {
+        files: ['app/js/**/*.js', 'app/css/*.css', 'app/views/**/*.html', 'server.js', 'models/*.js'],
+        tasks: ['test','build'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('build',['clean:dev','browserify:dev', 'copy:dev']);
+  grunt.registerTask('test', ['browserify:angulartest','karma:unit']);
+  grunt.registerTask('serve', ['express:dev','watch:express']);
+  grunt.registerTask('default',['test','build','serve']);
+
+};
