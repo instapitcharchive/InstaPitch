400fd0814091f2180b00abfd0d52e53feb36bd97
Mandi Grant <mandi018@gmail.com>
2014-09-25 10:21:34 -0700

    More auth progress

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+'use strict';
+
 var express = require('express');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var http = require('http');
+var passport = require('passport');
 var app = express();
 
 app.use(express.static(__dirname + '/build'));
 
+app.set('jwtTokenSecret', process.env.JWT_SECRET || 'developmentsecret');
+app.set('secret', process.env.SECRET || 'developmentsecret'); //passport requires this, but we don't actually use it
+
+app.use(passport.initialize());
+
+require('./lib/passport')(passport);
+var jwtauth = require('./lib/jwtauth')(app);
+
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/skills-development');
 
 app.use(bodyparser.json());
-require('./routes/admin-routes')(app);
+require('./routes/user-routes')(app, passport);
+require('./routes/admin-routes')(app, jwtauth.auth);
 
 var server = http.createServer(app);
 
